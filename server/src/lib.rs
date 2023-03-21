9c96cd1c40a01558df697339e2f3f2bc1dc5d2a6
michael <michael.dewey131@gmail.com>
2023-03-21 16:17:13 -0700

    Adds the worker abstraction so that we can pass code to spawned threads

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,20 @@
 use std::thread;
+
+struct Worker {
+    id: usize,
+    handle: thread::JoinHandle<()>
+}
+
+impl Worker {
+    fn new(id: usize) -> Self {
+        Self {
+            id,
+            handle: thread::spawn(|| {})
+        }
+    }
+}
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -14,11 +28,12 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
-            // Create some threads and store them in the vector
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            // Create some workers and store them in the vector
+            workers.push(Worker::new(id)) 
         } 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
     /// Another way to create a new ThreadPool which uses Result
     /// 
